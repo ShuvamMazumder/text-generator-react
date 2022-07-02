8f087409be353a529eae6860d7f71ea6c890e5d1
Shuvam Mazumder <shuvammaz94@gmail.com>
2022-07-02 22:21:38 +0600

    add new features

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,37 +1,56 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleUpClick= () =>{
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const handleOnChange= (event) =>{
-        setText(event.target.value);
-        //onChange func use korle ekta event genrate hoy.tar sahajje amra value change krte parbo.
-    }
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+    //onChange func use korle ekta event genrate hoy.tar sahajje amra value change krte parbo.
+  };
 
-    const [text,setText] = useState('enter text here');
+  const [text, setText] = useState("");
 
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="8"
-          value={text}
-          onChange= {handleOnChange}
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
 
-        ></textarea>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+        generate UpperCase
+        </button>
+      
+      <button className="btn btn-primary" onClick={handleLoClick}>
+        generate LowerCase
+      </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Continue to generate for Uppercase</button>
-    </div>
+      <div className="container my-3">
+        <h2>your text summary</h2>
+        <p>
+          {text.split(" ").length} words, {text.length} characters
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
-
 
 //Click -> button -> onClick()
 //Change -> TextArea -> onChange()
